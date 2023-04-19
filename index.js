36e5ec6526d17df6410c3105c8a3cf4a38da3e49
salman9464 <122408972+salman9464@users.noreply.github.com>
2023-04-19 16:36:17 +0330

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,274 @@
+var irancell="snip-irc.ddns.net";
+var mokhaberat="snip-mkh.ddns.net";
+
+var vmess=new Object();
+
+var vmess_general={
+    random1:{
+    name:"Random-1-Germany",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"d2722c54-5573-4804-9cea-2090ceab0c6c",
+    host:"sn-fn-2022.cf",
+    path:"/de2-vless.greenssh.xyz/vless"
+  },
+  random2:{
+    name:"Random-2-Turkey",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"735d2a0d-6618-4fad-bbb9-c2f0fec6dc68",
+    host:"sn-fn-2022.cf",
+    path:"/br1.vl.xservs.xyz/vless"
+    },
+    random3:{
+    name:"Random-3-India",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"3220f1a8-7a08-4313-8414-fdd2049dc3c3",
+    host:"sn-fn-2022.cf",
+    path:"/in1.vl.xservs.xyz/vless"
+    },
+    random4:{
+    name:"Random-4-France",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"b361a696-d525-4500-b01f-c5b02c64c89c",
+    host:"sn-fn-2022.cf",
+    path:"/fr1.vl.xservs.xyz/vless"
+    },
+    /*glitch_1:{
+    name:"glitch-1",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"65db41c0-496a-4d0d-b627-c64b216bd357",
+    host:"pixels-dolls-choose-size.trycloudflare.com",
+    path:'/argo-vless?ed=2048'
+    },
+    glitch_2:{
+    name:"glitch-2",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"0228a269-4862-4c0d-95ae-cd3e80ed859e",
+    host:"supported-feeding-cite-directors.trycloudflare.com",
+    path:'/argo-vless?ed=2048'
+    },
+    b4a_1:{
+    name:"back4app-1",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"846dc59e-64d3-443f-b186-3234c7efdeff",
+    host:"exhibition-pregnant-governments-americas.trycloudflare.com",
+    path:'/vless?ed=2048'
+    },
+    b4a_2:{
+    name:"back4app-2",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"d50c3a2c-46f6-4909-b3ca-9e50685b35d5",
+    host:"forbidden-expansys-grab-mentor.trycloudflare.com",
+    path:'/vless?ed=2048'
+    },*/
+    doprax_vless:{
+    name:"Doprax-General",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"94688b32-029d-4af1-85d2-f017ef257ce7",
+    host:"doprax.caraja6459.workers.dev",
+    path:'/vless'
+    },
+  amsterdam:{
+    name:"Amsterdam",
+    type:'vmess',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"53592AFD-BB7C-4759-8553-7BBEC9DE22C8",
+    host:"amsterdam.ponibif596.workers.dev",
+    path:"/speedtest"
+  },
+  kansas:{
+    name:"Kansas",
+    type:'vmess',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"BE7BB41C-E999-4A2B-B004-DE0577D347AA",
+    host:"kansas.rokoh37602.workers.dev",
+    path:"/speedtest"
+  },
+    lille:{
+    name:"Lille",
+    type:'vmess',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"3F81B9C2-2423-43D6-BBC0-C14D7048EDBA",
+    host:"lille.bodeman285.workers.dev",
+    path:"/speedtest"
+  }
+    /*seattle:{
+    name:"Seattle",
+    type:'vmess',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"5F8A9040-62D2-4CE8-8AF8-C84FE8097F17",
+    host:"seattle.xelir48549.workers.dev",
+    path:"/speedtest"
+    },
+    tokyo:{
+    name:"Tokyo",
+    type:'vmess',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"74829403-7C01-44BE-AF81-4A94AFBD1484",
+    host:"tokyo.meyoron664.workers.dev",
+    path:"/speedtest"
+    }*/
+    /*riga:{
+    name:"Riga",
+    type:'vmess',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"E63BE15E-7C4B-4A2C-A500-E367D4CA3FF3",
+    host:"general.sifame2627.workers.dev",
+    path:"/speedtest"
+  },*/
+};
+
+
+var vmess_private={
+
+  doprax1_vless:{
+    name:"Doprax-1",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"4f20446e-4c20-471c-9b2b-b6fe211de80a",
+    host:"doprax-private.vamog83176.workers.dev",
+    path:"/v2rayoxrp.eu-ggqrtyuigfhs.dopraxrocks.net/vless"
+  },
+  doprax2_vless:{
+    name:"Doprax-2",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"8da43bc1-5747-4e5a-a606-ca5fa2c9a8f0",
+    host:"doprax-private.vamog83176.workers.dev",
+    path:"/v2ray-2me5b.eu-ggqrtyuigfhs.dopraxrocks.net/vless"
+  },
+  render2:{
+    name:"Render-2-Ohio-Usa",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"18e27d9d-7862-47ef-9852-f5c5490aa511",
+    host:"render2-f03h.onrender.com",
+    path:"/"
+  },
+  render3_Singapore:{
+    name:"Render-3-Singapore",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"14aca844-ba16-4242-b59c-8c08a67d6ccd",
+    host:"render-3-zooq.onrender.com",
+    path:"/"
+  },
+  dusseldorf:{
+    name:"Dusseldorf",
+    type:'vmess',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"6DA14A8E-3014-41C8-8598-7949DB2E3A83",
+    host:"dusseldorf.xoteko5056.workers.dev",
+    path:"/speedtest"
+  },
+  replit1:{
+    name:"replit-1",
+    type:'vmess',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"31898681-d920-421c-979f-0bcaa8286df5",
+    host:"replit-india.lilowox351.workers.dev",
+    path:"/Salman.salman9464.repl.co/31898681-d920-421c-979f-0bcaa8286df5-vm"
+  },
+  replit2:{
+    name:"replit-2",
+    type:'vmess',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"1000f0cb-1927-4258-a072-0b3f3c9ba07b",
+    host:"replit-india.lilowox351.workers.dev",
+    path:"/replit-india-2.voyaf17201.repl.co/1000f0cb-1927-4258-a072-0b3f3c9ba07b-vm"
+  },
+render1:{
+    name:"Render-1-Frankfurt-Germany",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"aee69fad-3fe1-493e-8040-d572da4fac9c",
+    host:"render1-m8jz.onrender.com",
+    path:'/'
+    },
+    render3_Ohio:{
+    name:"Render-3-Oregon-Usa",
+    type:'vless',
+    ip_irc:irancell,
+    ip_mkh:mokhaberat,
+    id:"2fbb8fec-cd96-4e68-ae93-eb7bb0a77e7a",
+    host:"render-3-oregon.onrender.com",
+    path:'/'
+    }
+};
+export default {
+  async fetch(request) {
+    let vless=false;
+    let newConfigs='';
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/sn-gen')) {
+      vmess=vmess_general;
+    }else if(url.pathname.startsWith('/sn-7653')){
+      vmess=vmess_private;
+    }
+      for (var subConfig in vmess) {
+            let configNew1 = new Object();
+            //let configNew2 = new Object();
+            configNew1.v="2";
+            configNew1.port='443'
+            configNew1.add=vmess[subConfig].ip_mkh;
+            configNew1.id=vmess[subConfig].id;
+            configNew1.aid="0";
+            configNew1.scy="auto";
+            configNew1.net="ws";
+            configNew1.type="none";
+            configNew1.host=vmess[subConfig].host;
+            configNew1.tls="tls";
+            configNew1.sni=vmess[subConfig].host;
+            configNew1.alpn="http/1.1";
+            configNew1.path=vmess[subConfig].path;
+            /*configNew2=Object.assign({}, configNew1);
+            configNew2.add=vmess[subConfig].ip_irc;
+            configNew2.path=vmess[subConfig].path;*/ 
+
+            if(vmess[subConfig].type==='vmess'){
+              configNew1.ps=vmess[subConfig].name+'(Mokhaberat)';
+              //configNew2.ps=vmess[subConfig].name+'(Irancell)';
+              configNew1 = 'vmess://' + btoa(JSON.stringify(configNew1));
+              //configNew2 = 'vmess://' + btoa(JSON.stringify(configNew2));
+            }else if(vmess[subConfig].type==='vless'){
+              configNew1='vless://'+configNew1.id+'@'+configNew1.add+':'+ configNew1.port+'?encryption='+configNew1.type+'&security='+configNew1.tls+'&sni='+configNew1.sni+'&alpn=http%2F1.1&type=ws&host='+configNew1.host+'&path='+configNew1.path.replace('/','%2F')+'#'+vmess[subConfig].name+'(Mokhaberat)';
+              //configNew2='vless://'+configNew2.id+'@'+configNew2.add+':'+ configNew2.port+'?encryption='+configNew2.type+'&security='+configNew2.tls+'&sni='+configNew2.sni+'&alpn=http%2F1.1&type=ws&host='+configNew2.host+'&path='+configNew2.path.replace('/','%2F')+'#'+vmess[subConfig].name+'(Irancell)';
+            }
+
+            //newConfigs = newConfigs  + configNew1+ '\n'+configNew2+'\n';
+            newConfigs = newConfigs  + configNew1+ '\n';
+      }
+      return new Response(newConfigs);
+  },
+};
